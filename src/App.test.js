bbdc62c3beab5110686901b0edc244be8bf1efe7
iamshiv007 <Github@#007>
2023-12-22 17:14:04 +0530

    group testing with describe

--- src/App.test.js
+++ src/App.test.js
@@ -1,22 +1,68 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('test for first react app', () => {
-  render(<App />)
-  const appText = screen.getByText(/First REACT App/i)
+describe.skip("ui test group", () => {
+  test('test for first react app', () => {
+    render(<App />)
+    const appText = screen.getByText(/First REACT App/i)
 
-  expect(appText).toBeInTheDocument();
+    expect(appText).toBeInTheDocument();
 
-});
+  });
 
-test('test for about text', () => {
-  render(<App />)
-  const aboutText = screen.getByText("This project made for to learn testing in a react application")
-  expect(aboutText).toBeInTheDocument();
-});
+  test('test for about text', () => {
+    render(<App />)
+    const aboutText = screen.getByText("This project made for to learn testing in a react application")
+    expect(aboutText).toBeInTheDocument();
+  });
 
-test('test for react logo Image', () => {
-  render(<App />)
-  const reactLogo = screen.getByTitle("React logo image")
-  expect(reactLogo).toBeInTheDocument()
-});
+  test('test for react logo Image', () => {
+    render(<App />)
+    const reactLogo = screen.getByTitle("React logo image")
+    expect(reactLogo).toBeInTheDocument()
+  });
+})
+
+describe("api test group", () => {
+  test('test for first react app', () => {
+    render(<App />)
+    const appText = screen.getByText(/First REACT App/i)
+
+    expect(appText).toBeInTheDocument();
+
+  });
+
+  test('test for about text', () => {
+    render(<App />)
+    const aboutText = screen.getByText("This project made for to learn testing in a react application")
+    expect(aboutText).toBeInTheDocument();
+  });
+
+  test('test for react logo Image', () => {
+    render(<App />)
+    const reactLogo = screen.getByTitle("React logo image")
+    expect(reactLogo).toBeInTheDocument()
+  });
+
+  describe("home api group", () => {
+    test('test for first react app', () => {
+      render(<App />)
+      const appText = screen.getByText(/First REACT App/i)
+
+      expect(appText).toBeInTheDocument();
+
+    });
+
+    test('test for about text', () => {
+      render(<App />)
+      const aboutText = screen.getByText("This project made for to learn testing in a react application")
+      expect(aboutText).toBeInTheDocument();
+    });
+
+    test('test for react logo Image', () => {
+      render(<App />)
+      const reactLogo = screen.getByTitle("React logo image")
+      expect(reactLogo).toBeInTheDocument()
+    });
+  })
+})
